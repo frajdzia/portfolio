19874e1f4bda1e1f2e950c03f43b806b588ba321
frajdzia <s.lidia@interia.pl>
2024-04-27 22:34:11 +0100

    changes in files paths and added gallery for portfolio

--- index.js
+++ index.js
@@ -1,4 +1,45 @@
-document.querySelector('header').addEventListener('click', function() {
-    const currentTheme = document.body.getAttribute('data-theme');
-    document.body.setAttribute('data-theme', currentTheme === 'light' ? 'dark' : 'light');
-});
+// document.querySelector('header').addEventListener('click', function () {
+//     const currentTheme = document.body.getAttribute('data-theme');
+//     document.body.setAttribute('data-theme', currentTheme === 'light' ? 'dark' : 'light');
+// });
+
+function loadImages(type, count) {
+    // Clear existing images
+    thumbBar.innerHTML = '';
+
+    // Base folder or path for images
+    let basePath = "assets/portfolio_pictures/";
+
+    // Choose the pattern based on the type
+    let filenamePattern;
+    if (type === "india") {
+        filenamePattern = "india/india ";
+    } else if (type === "cambridge") {
+        filenamePattern = "cambridge/cambridge ";
+    }
+    else if (type === "london") {
+        filenamePattern = "london/london ";
+    }
+    else {
+        console.error("Invalid type specified");
+        return;
+    }
+
+    // Load and append images according to the specified count and type
+    for (let i = 1; i <= count; i++) {
+        const link = document.createElement('a');
+        link.className = 'gallery-link';
+        link.href = `${basePath}${type}/${type} (${i}).JPG`;
+
+        const figure = document.createElement('figure');
+        figure.className = 'gallery-image';
+
+        const image = document.createElement('img');
+        image.src = `${basePath}${type}/${type} (${i}).JPG`;
+        image.alt = `Gallery image ${i}`;
+
+        figure.appendChild(image);
+        link.appendChild(figure);
+        thumbBar.appendChild(link);
+    }
+}
